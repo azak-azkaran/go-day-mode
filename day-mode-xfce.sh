f0ea70dbb84d06978036e3379527367a0ccf1373
Andreas Wieland <andreas.wieland@ida-analytics.de>
2019-11-21 14:22:52 +0100

    GoDayMode - Add skript to get connected Screens:
    * added skript to get connected screens in bash

---
+++ day-mode-xfce.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# von: https://askubuntu.com/questions/414422/command-to-change-the-wallpaper-in-xubuntu
+
+connectedOutputs=$(xrandr | grep " connected" | sed -e "s/\([A-Z0-9]\+\) connected.*/\1/")
+activeOutput=$(xrandr | grep -e " connected [^(]" | sed -e "s/\([A-Z0-9]\+\) connected.*/\1/") 
+connected=$(echo $connectedOutputs | wc -w)
+
+xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor0/image-path -n -t string -s  ~/Pictures/1.jpeg
+xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitorLVDS1/workspace0/last-image -n -t string -s  ~/Pictures/1.jpeg
+
+for i in $(xfconf-query -c xfce4-desktop -p /backdrop -l|egrep -e "screen.*/monitor.*image-path$" -e "screen.*/monitor.*/last-image$"); do
+    xfconf-query -c xfce4-desktop -p $i -n -t string -s ~/Pictures/2.png  
+    xfconf-query -c xfce4-desktop -p $i -s ~/Pictures/2.png
+
+done
